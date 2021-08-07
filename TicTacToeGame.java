fd83acb3d83799e8fe108939a15f5979aa341a05
PavaniKempula <pavanikempula1998@gmail.com>
2021-08-07 13:57:06 +0530

    ADDED : Method for Creating Board Elements

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,11 +1,32 @@
 package com.bridgelabz;
-
+/*
+ * @author Pavani
+ * 
+ * purpose: TicTacToe program game where  player and computer can play the game
+ */
 public class TicTacToeGame {
 
+	// variables
+	private static char[] element;
+	
 	public static void main(String[] args) {
 
 		// Displaying TicTacTae Game Welcome Message
-		System.out.println("Welcome to Tic Tac Toe Game");
+		System.out.println("*------Welcome to Tic Tac Toe Game------*");
+		//for creating empty elements
+		boardCreation();
+	}	
+	
+	/*
+	 * creating elements with null
+	 * 0th index ignored for user friendly
+	 * @param element
+	 */
+	private static void boardCreation() {
+		element = new char[10];
+		for(int i = 1; i < 10; i++) {
+			element[i] = ' ';
+		}
+		System.out.println("Empty board created");
 	}
-
 }
